967a1ecc283651f582a5564b0f1832c883086e10
zhouqingping <714781770@qq.com>
2018-03-07 20:19:39 +0800

    add bubblesort

---
+++ bubblesort.py
@@ -0,0 +1,15 @@
+a = list(range(5, 0, -1))
+n = len(a)
+print(a)
+print('-'*30)
+for pass_num in r?!?jedi=0, ange(n-1, 0, -1):?!? (*_*start*_*, stop=None, step=1) ?!?jedi?!?
+    for i in range(pass_num):
+        if a[i]>a[i+1]:
+            temp = a[i+1]
+            a[i+1]=a[i]
+            a[i]=temp
+            print(a)
+    print(''*20)
+
+print('-'*30)
+print(a)
